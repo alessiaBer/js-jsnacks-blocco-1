8871b8fe43817572f1f57256905b872198dc3eda
alessiaBer <alexdeyn@gmail.com>
2023-03-14 11:43:14 +0100

    [add] first snack

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,3 +1,12 @@
 /* Snack 1 Blocco 1
 L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore. */
+
+const firstNumber = Number(prompt('inserisci il primo numero'));
+const secondNumber = Number(prompt('inserisci il secondo numero'));
+
+if (firstNumber > secondNumber) {
+    console.log(firstNumber)
+} else if(secondNumber > firstNumber) {
+    console.log(secondNumber)
+}
