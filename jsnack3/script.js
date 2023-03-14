9763e646413bdecfe5bf9b186f8df4f0887c2edb
alessiaBer <alexdeyn@gmail.com>
2023-03-14 12:51:45 +0100

    add snack 3

---
+++ jsnack3/script.js
@@ -0,0 +1,13 @@
+/* Snack 3
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti. */
+
+
+let sum = 0;
+
+for (let i = 0; i < 10; i++) {
+    let requestedNumber = Number(prompt('inserisci un numero'));
+    console.log(requestedNumber);
+    
+    sum += requested
+}
