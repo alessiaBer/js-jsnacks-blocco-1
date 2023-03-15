e6173a56447e35178c7502128aa35e7cb2500d05
alessiaBer <alexdeyn@gmail.com>
2023-03-15 12:25:35 +0100

    [fix] for loop of snack 3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -2,15 +2,16 @@
 Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti. */
 
-/* 
-let sum = 0;
+
+let sum1 = 0;
 
 for (let i = 0; i < 10; i++) {
     let requestedNumber = Number(prompt('inserisci un numero'));
-    console.log(requestedNumber);
     
-    sum += requested
-} */
+    sum1 += requestedNumber;
+} 
+
+console.log(sum1);
 
 /*Rifare con WHILE LOOP:  Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti. */
