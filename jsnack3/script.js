65bce9fc15e8dfa2767c080903ea411de21e7c71
alessiaBer <alexdeyn@gmail.com>
2023-03-15 11:53:44 +0100

    [add] snack3 with while

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -2,7 +2,7 @@
 Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti. */
 
-
+/* 
 let sum = 0;
 
 for (let i = 0; i < 10; i++) {
@@ -10,4 +10,21 @@ for (let i = 0; i < 10; i++) {
     console.log(requestedNumber);
     
     sum += requested
+} */
+
+/*Rifare con WHILE LOOP:  Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti. */
+
+let sum = 0;
+
+let x = 0;
+
+while (x < 10) {
+    let requestedNumber = Number(prompt('inserisci un numero'));
+    sum += requestedNumber;
+    
+    x++;
 }
+
+console.log(sum);
+
